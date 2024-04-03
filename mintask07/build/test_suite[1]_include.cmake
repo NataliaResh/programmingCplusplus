2ae69e5296b6757db909e2673349ec38480d6a0a
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-04-03 17:34:18 +0700

    delete build

--- mintask07/build/test_suite[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/natalia/programmingCplusplus/mintask07/build/test_suite[1]_tests.cmake")
-  include("/home/natalia/programmingCplusplus/mintask07/build/test_suite[1]_tests.cmake")
-else()
-  add_test(test_suite_NOT_BUILT test_suite_NOT_BUILT)
-endif()
--- mintask07/build/test_suite[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( Test0.Test0 /home/natalia/programmingCplusplus/mintask07/build/test_suite [==[--gtest_filter=Test0.Test0]==] --gtest_also_run_disabled_tests)
-set_tests_properties( Test0.Test0 PROPERTIES WORKING_DIRECTORY /home/natalia/programmingCplusplus/mintask07/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( test_suite_TESTS Test0.Test0)
